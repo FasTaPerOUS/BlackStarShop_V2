e178835e5212f3d1ee1652eb51ac17361b872023
FasTaPerOUS <emailFasTaPerOUS@users.noreply.github.com>
2021-07-28 19:51:37 +0300

    project init

---
+++ BlackStarShop_V2/BlackStarShop_V2/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BlackStarShop_V2
+//
+//  Created by Norik on 28.07.2021.
+//  Copyright © 2021 Norik. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.rootViewController = ...
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
